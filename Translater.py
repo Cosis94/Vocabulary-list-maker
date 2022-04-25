1ffe3dcbb6379751588bbb18cba905778381a1fd
Fatih Ekinci <Fatih.ekinci94@gmail.com>
2022-04-25 19:56:45 +0200

    Quick #2

--- Translater.py
+++ Translater.py
@@ -2,4 +2,6 @@ from googletrans import Translator
 translator = Translator()
 out = translator.translate('Hallo?',src="de",dest="ja")
 
+#Fatih ist cool
+
 print(u'\u30af\u30a4\u30c3\u30af\u30b9\u30bf\u30fc\u30c8\u30ac\u30a4\u30c9\u30A0')
